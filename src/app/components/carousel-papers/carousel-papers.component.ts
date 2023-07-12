dc933f7ac939f92fda828f24643c0af53a8000d7
stephanogiuseppe <stephano.grp@gmail.com>
2023-07-12 13:06:13 -0300

    [WIP]

--- src/app/components/carousel-papers/carousel-papers.component.ts
+++ src/app/components/carousel-papers/carousel-papers.component.ts
@@ -1,71 +1,52 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AnimationEvent } from '@angular/animations';
- import { trigger, state, style, transition, animate } from '@angular/animations';
-
-import {TreeNode} from 'primeng/api';
 
 interface Paper {
   id?: string;
-  name?: string;
-  description?: string;
+  title?: string;
   image?: string;
+  linkSrc?: string;
 }
 
 @Component({
   selector: 'app-carousel-papers',
   templateUrl: './carousel-papers.component.html',
-  styleUrls: ['./carousel-papers.component.scss']
+  styleUrls: ['./carousel-papers.component.scss'],
 })
 export class CarouselPapersComponent implements OnInit {
-
   papers: Paper[] = [];
 
-    constructor() { }
-
-    ngOnInit() {
-        this.papers = [
-          {
-            id: '1',
-            name: 'Project 1',
-            description: 'A Systematic Dataset for 5G UAV',
-            image: 'assets/img/job/paper1.png',
-          },
-          {
-            id: '2',
-            name: 'Project 2',
-            description: 'Another Theme Here ',
-            image: 'assets/img/job/paper2.png',
-          },
-          {
-            id: '3',
-            name: 'Project 3',
-            description: 'Another Theme Here 2',
-            image: 'assets/img/job/paper1.png',
-          },
-          {
-            id: '4',
-            name: 'Project 5',
-            description: 'Another Theme Here 2',
-            image: 'assets/img/job/paper1.png',
-          },
-          {
-            id: '5',
-            name: 'Project 5',
-            description: 'Another Theme Here 2',
-            image: 'assets/img/job/paper1.png',
-          },
-          {
-            id: '6',
-            name: 'Project 6',
-            description: 'Another Theme Here 2',
-            image: 'assets/img/job/paper1.png',
-          },
-          {
-            id: '7',
-            name: 'Project 7',
-            description: 'Another Theme Here 2',
-            image: 'assets/img/job/paper1.png',
-          },
-        ];
-    }
+  ngOnInit() {
+    this.papers = [
+      {
+        id: '1',
+        title: 'Deep Attention Recognition for Attack Identification in 5G UAV scenarios: Novel Architecture and End-to-End Evaluation',
+        image: 'assets/img/job/paper1.png',
+        linkSrc: 'https://arxiv.org/pdf/2303.12947.pdf',
+      },
+      {
+        id: '2',
+        title: 'Increasing Reliability on UAV Fading Scenarios',
+        image: 'assets/img/job/paper2.png',
+        linkSrc: 'https://ieeexplore.ieee.org/document/9706194',
+      },
+      {
+        id: '3',
+        title: 'A Synthetic Dataset for 5G UAV Attacks Based on Observable Network Parameters',
+        image: 'assets/img/job/paper1.png',
+        linkSrc: 'https://arxiv.org/abs/2211.09706',
+      },
+      {
+        id: '4',
+        title: 'A Scalable LDPC Coding Scheme for Adaptive HARQ Techniques',
+        image: 'assets/img/job/paper1.png',
+        linkSrc: 'https://ieeexplore.ieee.org/document/9860855',
+      },
+      {
+        id: '5',
+        title: 'Latent Space Transformers for Generalizing Deep Networks',
+        image: 'assets/img/job/paper1.png',
+        linkSrc: 'https://ieeexplore.ieee.org/document/9686099',
+      },
+    ];
+  }
 }
